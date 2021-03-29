bce4435ba9db60ec43047d4590aac404dba10e23
li-zhuofan-350562 <350888562@qq.com>
2021-03-29 15:36:25 +0800

    update

--- src/Main2.java
+++ src/Main2.java
@@ -4,43 +4,39 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * @author hunnu/lzf
- * @Date 2021/3/10
+ 给你一个 严格升序排列的正整数数组 arr和一个整数k。
+
+ 请你找到这个数组里第k个缺失的正整数。
+
+
+ 示例 1：
+
+ 输入：arr = [2,3,4,7,11], k = 5
+ 输出：9
+ 解释：缺失的正整数包括 [1,5,6,8,9,10,12,13,...] 。第 5 个缺失的正整数为 9 。
+ 示例 2：
+
  */
 public class Main2 {
-	private final static int N=200;
-	private static BitSet bitSet;
-
 	public static void main(String[] args) {
-		try(Scanner in = new Scanner(System.in)){
-			String string = in.next();
-			int c = string.indexOf(",");
-			int[][] matrix = new int[c][c];
-			for (int i = 0; i < c; i++) {
-				for (int j = 0; j <c ; j++) {
-					matrix[i][j]=Integer.parseInt(string.substring(i*(c+1)+j,i*(c+1)+j+1));
-				}
-			}
-			setRound(matrix,c);
-			int maxN=fun(matrix,c);
-			System.out.println(maxN);
-		}
+		int[] arr = new int[]{2,3,4,7,11};
+		int k=5;
+		System.out.println(f(arr,k));
 	}
-	private static int fun(int[][] matrix,int c){
+	public static int f(int[] arr,int k){
+		boolean flag=false;
 		int n=0;
-		for (int i = 0; i < c; i++) {
-			if (!bitSet.get(i)){
+		int num = 1;
+		for (int index=0; !flag ; num++) {
+			if (num>=arr[index]){
+				index++;
+			}else {
 				n++;
-				bitSet.set(i);
+			}
+			if (n==k){
+				flag=true;
 			}
 		}
-		return n;
-	}
-	private static void setRound(int[][] matrix,int c) {
-		for (int i = 0; i < c; i++) {
-			for (int j = 0; j < c; j++) {
-				
-			}
-		}
+		return num-1;
 	}
 }
