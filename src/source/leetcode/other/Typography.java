0ee5267dc7c1eb9edefa5731eedaba03f1d61679
lzf350888562 <350888562@qq.com>
2022-06-09 19:41:08 +0800

    忽略的情况

--- src/source/leetcode/other/Typography.java
+++ src/source/leetcode/other/Typography.java
@@ -1,15 +1,13 @@
 package source.leetcode.other;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringJoiner;
+import java.util.*;
 
 /**
  * 面试题, 见readme no.1
  */
 public class Typography {
-    private static Set<String> res = new HashSet<>();
-    private static int N = 4;
+    private static List<String> res = new ArrayList<>();
+    private static final int N = 3;
     public static void main(String[] args) {
         int[][] frm = new int[N][N];
         for (int i = 0; i < frm.length; i++) {
@@ -32,10 +30,32 @@ public class Typography {
                 }
             }
             res.add(stringJoiner.toString());
+            if(!isLineOrSquare(frm)){
+                res.add(stringJoiner.toString() + "(共享坐标)");
+            }
         }else{
             setFrm(frm, i, j+1, n);
             setFrm(frm, i + 1, 0, n);
         }
         frm[i][j] = 0;
+    }
+
+    public static boolean isLineOrSquare(int[][] frm){
+        if(frm[N-1][0] == 1 || frm[0][N-1] == 1){
+            return true;
+        }
+        int sqrt = (int)Math.sqrt(N);
+        int nTmp = sqrt * sqrt;
+        if(N == nTmp){
+            for (int i = 0; i < sqrt; i++) {
+                for (int j = 0; j < sqrt; j++) {
+                    if(frm[i][j] == 0){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
